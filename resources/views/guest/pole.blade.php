5c588a7c440a27e29b6988224b6863ee1a6f4e7c
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2024-09-04 08:39:45 +0700

    edit title

--- resources/views/guest/pole.blade.php
+++ resources/views/guest/pole.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <meta charset="utf-8">
-    <title>CM : หลักระดับนน้ำท่วม </title>
+    <title>CM : หลักระดับน้ำท่วม </title>
 
     <link rel="icon" href="{{ asset('images/icon/favicon1.ico')}}" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Mitr|Prompt" rel="stylesheet">
