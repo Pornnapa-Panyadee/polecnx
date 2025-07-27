5d7d624624c0e9c8f5ff9f08445905c508f611fc
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2025-07-27 22:59:40 +0700

    path /cmflood

--- public/js/tabledata/map-floodmark.js
+++ public/js/tabledata/map-floodmark.js
@@ -203,7 +203,7 @@ function loadAllData(mo = 1) {
     for (let level = 1; level <= 5; level++) {
         const layer = stationLayers[level];
         // const url = `/flood24/getDataSurveyLevel/${level}`; // อย่าใช้ {{ }} ถ้าอยู่นอก Blade
-        const url = `/getDataSurveyLevelVer1/${level}`;
+        const url = `/cmflood/getDataSurveyLevelVer1/${level}`;
 
         $.ajax({
             url: url,
